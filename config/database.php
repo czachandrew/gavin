88efa1e0f075cb2e237a9ea51b1da63042095c06
czachandrew <czachandrew@gmail.com>
2017-03-22 16:55:52 -0500

    Updating the config

--- config/database.php
+++ config/database.php
@@ -41,14 +41,14 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => 'localhost:8889',
-            //'port' => env('DB_PORT', '3306'),
-            'database' => 'autism',
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'forge'),
             'username' => 'root',
             'password' => 'root',
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
-            'unix_socket'   => '/Applications/MAMP/tmp/mysql/mysql.sock',
+            
             'prefix' => '',
             'strict' => true,
             'engine' => null,
